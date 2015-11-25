f8808203dfe709a4647f447f99a314b4fe9972f5
Administrator <sms247247@gmail.com>
2015-11-25 15:46:05 +0500

    third tab

--- app/src/main/java/com/home/groupsms/TabFragmentContacts.java
+++ app/src/main/java/com/home/groupsms/TabFragmentContacts.java
@@ -3,6 +3,7 @@ package com.home.groupsms;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
